c9b559c513717245e04e39ef0e6a2f5461389552
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-20 15:35:38 -0600

    Installed Node package

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
 // swift-tools-version:4.0
+
 import PackageDescription
+
 let package = Package(
     name: "Countries",
     products: [
         .library(name: "Countries", targets: ["Countries"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/node.git", .exact("2.1.1"))
     ],
     targets: [
-        .target(name: "Countries", dependencies: []),
+        .target(name: "Countries", dependencies: ["Node"]),
         .testTarget(name: "CountriesTests", dependencies: ["Countries"]),
     ]
 )
