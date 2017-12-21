57f937f5d91189ca71da5277ed922ca5df5d1970
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-21 14:49:01 -0600

    Made Node dependency version looser

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Countries", targets: ["Countries"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/node.git", .exact("2.1.1"))
+        .package(url: "https://github.com/vapor/node.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         .target(name: "Countries", dependencies: ["Node"]),
