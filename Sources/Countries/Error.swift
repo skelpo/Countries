a2da52a1efcc9aefe708dc921f7aa8a0c190623b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-22 08:51:10 -0600

    Conformed CountryError to CustomStringConvertible

--- Sources/Countries/Error.swift
+++ Sources/Countries/Error.swift
@@ -1,3 +1,9 @@
-public enum CountryError: Error {
+public enum CountryError: Error, CustomStringConvertible {
     case unsupportedCountryCode(String)
+    
+    public var description: String {
+        switch self {
+        case let .unsupportedCountryCode(code): return "There was no country if the code '\(code)' found"
+        }
+    }
 }
