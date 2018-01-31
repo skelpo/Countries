ae7caf4c3fe2958466633ab5f3ba8d1315f70334
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-31 09:15:10 -0600

    Conformed CountryError to Debuggable

--- Sources/Countries/Error.swift
+++ Sources/Countries/Error.swift
@@ -1,9 +1,21 @@
-public enum CountryError: Error, CustomStringConvertible {
+import Debugging
+
+public enum CountryError: Error, Debuggable, CustomStringConvertible {
     case unsupportedCountryCode(String)
     
     public var description: String {
         switch self {
         case let .unsupportedCountryCode(code): return "No country with the code '\(code)' was found"
+        }
+    }
+    
+    public var reason: String {
+        return self.description
+    }
+    
+    public var identifier: String {
+        switch self {
+        case .unsupportedCountryCode: return "unsupportedCountryCode"
         }
     }
 }
