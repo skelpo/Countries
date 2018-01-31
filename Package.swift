d2183119f6d346ca0e2b1676e8052ec6897a006e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-31 09:00:57 -0600

    Replaced Node dependency with Core

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
         .library(name: "Countries", targets: ["Countries"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/node.git", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/vapor/core.git", .branch("beta"))
     ],
     targets: [
-        .target(name: "Countries", dependencies: ["Node"]),
+        .target(name: "Countries", dependencies: ["Bits"]),
         .testTarget(name: "CountriesTests", dependencies: ["Countries"]),
     ]
 )
