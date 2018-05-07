9600027da005a551448266c1a90ab42b3acac7f8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-07 14:31:12 -0500

    Updated Vapor version to official release

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Countries", targets: ["Countries"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "Countries", dependencies: ["Debugging", "Core"]),
